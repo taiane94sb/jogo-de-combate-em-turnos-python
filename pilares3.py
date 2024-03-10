3380e27077790f55e56d332cf43176f43b271528
taiane94sb <taiane19sb94@gmail.com>
2024-03-10 10:06:41 -0300

    pilares do poo - abstração

---
+++ pilares3.py
@@ -0,0 +1,30 @@
+# Exemplo de encapsulamento
+print("\nExemplo de abstração:")
+
+from abc import ABC, abstractmethod
+
+
+class Veiculo(ABC):
+    @abstractmethod
+    def ligar(self):
+        pass
+
+    @abstractmethod
+    def desligar(self):
+        pass
+
+
+class Carro(Veiculo):
+    def __init__(self) -> None:
+        pass
+
+    def ligar(self):
+        return "Carro ligado usando a chave"
+
+    def desligar(self):
+        return "Carro desligado usando a chave"
+
+
+carro_amarelo = Carro()
+print(carro_amarelo.ligar())
+print(carro_amarelo.desligar())
